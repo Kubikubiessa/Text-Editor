63072b5ce900be75e8fedb74ded080bce7b69249
Heidrun Kubiessa <heidrunkubiessa@heidruns-air.lan>
2023-02-10 12:29:03 -0700

    build webpack

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -25,15 +25,15 @@ export const putDb = async (content) => {
   // Open up the desired object store.
   const store = tx.objectStore("jate");
   // Use the .put() method on the store and pass in the content.
-  const request = store.put({ id: id, jate: content });
+  const request = store.put({ id: 1, jate: content });
   // Get confirmation of the request.
   const result = await request;
   console.log("🚀 - data saved to the database", result);
   //send error if putDb does not work
-  console.error("putDb not implemented");
+  //console.error("putDb not implemented");
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log("GET from the database");
   // Create a connection to the database database and version we want to use.
@@ -46,10 +46,9 @@ export const getDb = async () => {
   const request = store.getAll();
   // Get confirmation of the request.
   const result = await request;
+
   console.log("result.value", result);
   return result;
-
-  console.error("getDb not implemented");
 };
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,72 +1,73 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 // Add and configure workbox plugins for a service worker and manifest file.✅
 //CSS loaders and babel to webpack.✅
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     //single entry to webpack compiler
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
- 
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
-      // Webpack plugin that generates our html file and injects our bundles. 
+      // Webpack plugin that generates our html file and injects our bundles.
       new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'Just Another Text Editor'
+        template: "./index.html",
+        title: "Just Another Text Editor",
       }),
-       // Injects custom service worker
+      // Injects custom service worker
       new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'src-sw.js',
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
       }),
-        // Creates a manifest.json file, which is a form of instruction of how to install the app
-        new WebpackPwaManifest({
-          fingerprints: false,
-          inject: true,
-          name: 'Just Another Text Editor',
-          short_name: 'Text Editor',
-          description: 'Know how to write your prose!',
-          background_color: '#0a2e37',
-          theme_color: '#225ca3',
-          start_url: './',
-          publicPath: './',
-          icons: [
-            {
-              src: path.resolve('src/images/logo.png'),
-              sizes: [96, 128, 192, 256, 384, 512],
-              destination: path.join('assets', 'icons'),
-            },
-          ],
-        }),
-      
+      // Creates a manifest.json file, which is a form of instruction of how to install the app
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Just Another Text Editor",
+        short_name: "Text Editor",
+        description: "Know how to write your prose!",
+        background_color: "#0a2e37",
+        theme_color: "#225ca3",
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
-//modules and loaders
+    //modules and loaders
     module: {
       rules: [
         {
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+          use: ["style-loader", "css-loader"],
         },
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
           // We use babel-loader in order to use ES6.
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
             },
           },
         },
